be51ea9ea36d2ba9bdf7a68f6f213ffb98abaec1
Grigoriy Abashkin <i7421855@gmail.com>
2019-04-29 00:05:11 +0600

    work: добавил обработку сообщений

--- AssistantTelegramBot/Program.cs
+++ AssistantTelegramBot/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
 
 namespace AssistantTelegramBot
 {
     class Program
     {
+        private static readonly TelegramBotClient bot = new TelegramBotClient("825644646:AAG2dgY8DmeEZtkUhsMZx4boJNCvg-TSfDQ");
+
         static void Main(string[] args)
         {
+            bot.OnMessage += answer;
+
+            bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.ReadLine();
+            bot.StopReceiving();
+        }
+
+        private static void answer(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message.Text == "/reply")
+            {
+                bot.SendTextMessageAsync(message.Chat.Id, "hi");
+            }
+            else
+            {
+                bot.SendTextMessageAsync(message.Chat.Id, message.Text);
+            }
         }
     }
 }
