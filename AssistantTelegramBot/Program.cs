fec64f36336d55f955e03c2d2109a55920003648
Grigoriy Abashkin <i7421855@gmail.com>
2019-05-06 19:05:10 +0600

    work: добавил подключение к sql.

--- AssistantTelegramBot/Program.cs
+++ AssistantTelegramBot/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
@@ -14,6 +17,7 @@ namespace AssistantTelegramBot
         {
             bot.OnMessage += answer;
 
+            Console.WriteLine(connectingDb());
             bot.StartReceiving(Array.Empty<UpdateType>());
             Console.ReadLine();
             bot.StopReceiving();
@@ -25,25 +29,6 @@ namespace AssistantTelegramBot
             switch (message.Text) {
                 case "/reply":
                     bot.SendTextMessageAsync(message.Chat.Id, "hi");
-                    break;
-                case "/keyboard":
-                    bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
-                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("1"),
-                            InlineKeyboardButton.WithCallbackData("2"),
-                            InlineKeyboardButton.WithCallbackData("3")
-                        },
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("4"),
-                            InlineKeyboardButton.WithCallbackData("5"),
-                            InlineKeyboardButton.WithCallbackData("6")
-                        },
-                    });
-                    bot.SendTextMessageAsync(message.Chat.Id, "ok", replyMarkup: inlineKeyboard);
                     break;
                 case "/keyboardCustom":
                     ReplyKeyboardMarkup keyboardMarkup = new[]
@@ -58,6 +43,32 @@ namespace AssistantTelegramBot
                     break;
             }
         
+        }
+
+        private static string connectingDb()
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS; Initial Catalog=AssistantDb; Integrated Security=True";
+            string SelectAllTable = @"SELECT * FROM Theme
+                                      SELECT * FROM Question
+                                      SELECT * FROM Answer";
+
+            string themes = "1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(SelectAllTable, connection);
+                adapter.TableMappings.Add("Table",  "Theme");
+                adapter.TableMappings.Add("Table1", "Question");
+                adapter.TableMappings.Add("Table2", "Answer");
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                foreach (DataRow i in dataSet.Tables["Theme"].Rows)
+                {
+                    themes = i["Name"].ToString();
+                }
+            }
+            return themes;
         }
     }
 }
